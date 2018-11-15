1cefa1675f0723653aef98929f0395e71209476b
dikey <DikeyWang@163.com>
2018-11-15 08:20:07 +0800

    2018.11.13数据结构节点练习

--- src/main/java/com/xtkj/hanoi.java
+++ src/main/java/com/xtkj/hanoi.java
@@ -2,13 +2,14 @@ package com.xtkj;
 
 public class hanoi {
     public static void main(String[] args) {
-        hanoi(3, 'A', 'B', 'C');
+        hanoi(100, 'A', 'B', 'C');
     }
 
     /**
      * 无论有多少个盘子，都只认为有两个盘子，上面的和最下面的
      */
     public static void hanoi(int i, char left, char mid, char right) {
+
         if (i == 1) {
             System.out.println("将盘子1从" + left + "移动至" + right);
         } else {
